51446498955fcc7621f1c61d527483a2fc791b85
andresgc04 <andresgc1997@outlook.com>
2021-04-06 00:27:30 -0400

    CreacionModa

---
+++ ModaMedianaYMediaAritmetica/ModaMedianaYMediaAritmetica/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ModaMedianaYMediaAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arregloModa = new int[] { 19, 8, 29, 35, 19, 28, 15,19 };
+
+            int[] listadoArregloModa = new int[arregloModa.Length];
+
+            Array.Sort(arregloModa);
+
+   
+
+            for (int i = 0; i < arregloModa.Length; i++)
+            {
+                int contadorModa = 0;
+
+                for (int j = 0; j < arregloModa.Length; j++)
+                {
+                    if (arregloModa[i] == arregloModa[j])
+                    {
+                        contadorModa++;
+                        if (numero(arregloModa[i]))
+                        {
+                            listadoArregloModa[i] = arregloModa[i];
+                        }
+                    }
+                }
+                if (listadoArregloModa[i] != 0)
+                {
+                    Console.WriteLine(listadoArregloModa[i] + " Aparece: " + contadorModa);
+                }
+            }
+            bool numero(int num)
+            {
+                for (int i = 0; i < listadoArregloModa.Length; i++)
+                {
+                    if (listadoArregloModa[i] == num)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
